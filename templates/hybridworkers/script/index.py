0990810919da6bdc67bf4b63f3b1c0517865c596
Kamilo Amir <kamilo@gmail.com>
2025-03-14 16:51:15 -0400

    added functionality for hybrid workers

---
+++ templates/hybridworkers/script/index.py
@@ -0,0 +1,61 @@
+functionWorkerShutdown:
+  Type: AWS::Serverless::Function
+  Properties:
+    Description: Cribl Stream Worker Shutdown
+    InlineCode: |
+      import boto3
+      import logging
+      import json
+      import time
+      import os
+
+      ssm = boto3.client('ssm')
+      autoscaling = boto3.client('autoscaling')
+
+      log = logging.getLogger()
+      log.setLevel(logging.DEBUG)
+
+      def handler(event, context):
+          log.debug("Received event {}".format(json.dumps(event)))
+          
+          command = ssm.send_command(
+              InstanceIds=[event['detail']['EC2InstanceId']],
+              DocumentName=os.environ['SSM_DOCUMENT_NAME'],
+              Comment='Gracefully terminate Cribl Stream worker node',
+              Parameters={
+                  "commands": ["systemctl disable cribl", "systemctl daemon-reload", "systemctl stop cribl"],
+                  "workingDirectory": [""],
+                  "executionTimeout": ["3600"]
+              },
+              CloudWatchOutputConfig={
+                  'CloudWatchOutputEnabled': True
+              }
+          )
+
+          command_id = command['Command']['CommandId']
+          for x in range(60):
+              time.sleep(10)
+              response = ssm.get_command_invocation(
+                  CommandId=command_id,
+                  InstanceId=event['detail']['EC2InstanceId']
+              )
+              if response.get('Status') == 'Success':
+                  break
+              elif response.get('Status') in ['Failed', 'Cancelled', 'TimedOut']:
+                  log.error(f"SSM command failed: {response.get('StatusDetails')}")
+                  return
+
+          autoscaling.complete_lifecycle_action(
+              LifecycleHookName=event['detail']['LifecycleHookName'],
+              AutoScalingGroupName=event['detail']['AutoScalingGroupName'],
+              LifecycleActionToken=event['detail']['LifecycleActionToken'],
+              LifecycleActionResult='CONTINUE',
+              InstanceId=event['detail']['EC2InstanceId']
+          )
+
+    Runtime: python3.12
+    Timeout: 600
+    Handler: index.handler
+    Environment:
+      Variables:
+        SSM_DOCUMENT_NAME: AWS-RunShellScript
